3e3a34f2ff9ff1b041f5291acccdd9b0be1f5d35
Denis Grachev <justwantpost@gmail.com>
2025-08-12 17:17:57 +0200

    commit my changes

--- scripts/test/test_grasp.sh
+++ scripts/test/test_grasp.sh
@@ -2,7 +2,9 @@
 # set -x
 gpu=0
 
-model_path=a2_pretrained/checkpoints/sl_checkpoint_199.pth
+# model_path=logs/a2_pretrained/checkpoints/sl_checkpoint_199.pth
+# model_path=logs/2025-07-20-17-52-02-train-a2/checkpoints/sl_checkpoint_2025-07-20_23-01-50_199.pth
+model_path=logs/2025-07-28-12-08-39-train-a2/checkpoints/sl_checkpoint_2025-07-28_18-39-32_249.pth
 log=a2
 
 echo $model_path
